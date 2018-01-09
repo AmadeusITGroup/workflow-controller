4a628c8dd8d8366f8c74f3e86b7cb101380d274b
Salvatore Dario Minonne <salvatore-dario.minonne@amadeus.com>
2018-01-09 14:24:47 +0100

    generated code

--- pkg/client/informers/externalversions/workflow/v1/workflow.go
+++ pkg/client/informers/externalversions/workflow/v1/workflow.go
@@ -5,6 +5,8 @@
 package v1
 
 import (
+	time "time"
+
 	workflow_v1 "github.com/amadeusitgroup/workflow-controller/pkg/api/workflow/v1"
 	internalinterfaces "github.com/amadeusitgroup/workflow-controller/pkg/client/informers/externalversions/internalinterfaces"
 	v1 "github.com/amadeusitgroup/workflow-controller/pkg/client/listers/workflow/v1"
@@ -13,7 +15,6 @@ import (
 	runtime "k8s.io/apimachinery/pkg/runtime"
 	watch "k8s.io/apimachinery/pkg/watch"
 	cache "k8s.io/client-go/tools/cache"
-	time "time"
 )
 
 // WorkflowInformer provides access to a shared informer and lister for
@@ -24,19 +25,34 @@ type WorkflowInformer interface {
 }
 
 type workflowInformer struct {
-	factory internalinterfaces.SharedInformerFactory
+	factory          internalinterfaces.SharedInformerFactory
+	tweakListOptions internalinterfaces.TweakListOptionsFunc
+	namespace        string
 }
 
 // NewWorkflowInformer constructs a new informer for Workflow type.
 // Always prefer using an informer factory to get a shared informer instead of getting an independent
 // one. This reduces memory footprint and number of connections to the server.
 func NewWorkflowInformer(client versioned.Interface, namespace string, resyncPeriod time.Duration, indexers cache.Indexers) cache.SharedIndexInformer {
+	return NewFilteredWorkflowInformer(client, namespace, resyncPeriod, indexers, nil)
+}
+
+// NewFilteredWorkflowInformer constructs a new informer for Workflow type.
+// Always prefer using an informer factory to get a shared informer instead of getting an independent
+// one. This reduces memory footprint and number of connections to the server.
+func NewFilteredWorkflowInformer(client versioned.Interface, namespace string, resyncPeriod time.Duration, indexers cache.Indexers, tweakListOptions internalinterfaces.TweakListOptionsFunc) cache.SharedIndexInformer {
 	return cache.NewSharedIndexInformer(
 		&cache.ListWatch{
 			ListFunc: func(options meta_v1.ListOptions) (runtime.Object, error) {
+				if tweakListOptions != nil {
+					tweakListOptions(&options)
+				}
 				return client.WorkflowV1().Workflows(namespace).List(options)
 			},
 			WatchFunc: func(options meta_v1.ListOptions) (watch.Interface, error) {
+				if tweakListOptions != nil {
+					tweakListOptions(&options)
+				}
 				return client.WorkflowV1().Workflows(namespace).Watch(options)
 			},
 		},
@@ -46,12 +62,12 @@ func NewWorkflowInformer(client versioned.Interface, namespace string, resyncPer
 	)
 }
 
-func defaultWorkflowInformer(client versioned.Interface, resyncPeriod time.Duration) cache.SharedIndexInformer {
-	return NewWorkflowInformer(client, meta_v1.NamespaceAll, resyncPeriod, cache.Indexers{cache.NamespaceIndex: cache.MetaNamespaceIndexFunc})
+func (f *workflowInformer) defaultInformer(client versioned.Interface, resyncPeriod time.Duration) cache.SharedIndexInformer {
+	return NewFilteredWorkflowInformer(client, f.namespace, resyncPeriod, cache.Indexers{cache.NamespaceIndex: cache.MetaNamespaceIndexFunc}, f.tweakListOptions)
 }
 
 func (f *workflowInformer) Informer() cache.SharedIndexInformer {
-	return f.factory.InformerFor(&workflow_v1.Workflow{}, defaultWorkflowInformer)
+	return f.factory.InformerFor(&workflow_v1.Workflow{}, f.defaultInformer)
 }
 
 func (f *workflowInformer) Lister() v1.WorkflowLister {
--- pkg/client/versioned/typed/workflow/v1/fake/fake_workflow.go
+++ pkg/client/versioned/typed/workflow/v1/fake/fake_workflow.go
@@ -83,18 +83,6 @@ func (c *FakeWorkflows) Update(workflow *workflow_v1.Workflow) (result *workflow
 	return obj.(*workflow_v1.Workflow), err
 }
 
-// UpdateStatus was generated because the type contains a Status member.
-// Add a +genclient:noStatus comment above the type to avoid generating UpdateStatus().
-func (c *FakeWorkflows) UpdateStatus(workflow *workflow_v1.Workflow) (*workflow_v1.Workflow, error) {
-	obj, err := c.Fake.
-		Invokes(testing.NewUpdateSubresourceAction(workflowsResource, "status", c.ns, workflow), &workflow_v1.Workflow{})
-
-	if obj == nil {
-		return nil, err
-	}
-	return obj.(*workflow_v1.Workflow), err
-}
-
 // Delete takes name of the workflow and deletes it. Returns an error if one occurs.
 func (c *FakeWorkflows) Delete(name string, options *v1.DeleteOptions) error {
 	_, err := c.Fake.
